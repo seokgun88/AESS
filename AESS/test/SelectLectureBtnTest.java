34430318a682bc75ed41ab813b57180b0dc372b0
Yeongseok Lee <sukgun28@naver.com>
2015-05-13 23:12:48 +0900

    교수 강의목록 버튼 기능 정상화

---
+++ AESS/test/SelectLectureBtnTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import java.sql.*;
+
+import org.junit.*;
+
+
+public class SelectLectureBtnTest {
+
+	@Test
+	public void test() {
+		DBconnect dbcon = new DBconnect();
+		Connection conn = dbcon.connect();
+		GUI_ProfessorTable profTable = new GUI_ProfessorTable(conn, "P");
+		profTable.resetProfessorTable();
+		assertEquals(true,profTable.isProfTable);
+	}
+
+}
