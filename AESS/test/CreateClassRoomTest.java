7ac8075e1efd8ebf240b4d32f01e288ea1996cf7
Yeongseok Lee <sukgun28@naver.com>
2015-06-18 01:09:40 +0900

    createClassroom 테스트케이스 수정

--- AESS/test/CreateClassRoomTest.java
+++ AESS/test/CreateClassRoomTest.java
@@ -8,62 +8,62 @@ public class CreateClassRoomTest {
 	
 	@Test
 	public void CreateClassRoomTest1() {
-		assertEquals(false, User_Admin.CreateClassRoom("410", "  ", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("410", "  ", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest2() {
-		assertEquals(false, User_Admin.CreateClassRoom("410", "9", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("410", "9", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest3() {
-		assertEquals(false, User_Admin.CreateClassRoom("410", "", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("410", "", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest4() {
-		assertEquals(false, User_Admin.CreateClassRoom("410", "#E9", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("410", "#E9", "50" , "bim mic" ) );
 	}
 
 	@Test
 	public void CreateClassRoomTest5() {
-		assertEquals(false, User_Admin.CreateClassRoom("4a10", "E9", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("4a10", "E9", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest6() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "E9", "a50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "E9", "a50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest7() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest8() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", " ", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", " ", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest9() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "1", "50" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "1", "50" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest10() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "E9", "" , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "E9", "" , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest11() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "E9", " " , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "E9", " " , "bim mic" ) );
 	}
 	
 	@Test
 	public void CreateClassRoomTest12() {
-		assertEquals(false, User_Admin.CreateClassRoom("411", "E9", "50 " , "bim mic" ) );
+		assertEquals(false, User_Admin.createClassRoom("411", "E9", "50 " , "bim mic" ) );
 	}
 	
 }
